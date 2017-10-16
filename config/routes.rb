2a166b3f1a79532ef6325f1fc690bda6a9bf8091
miyuki <mo17304004@ga.tera-house.ac.jp>
2017-10-16 11:59:50 +0000

    ファイルをつくった

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  
+  get 'boad' => 'boad#index'
+  
+  get 'top/index'
+  
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
